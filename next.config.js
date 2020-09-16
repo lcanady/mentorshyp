3140b384071d7f81586a090115ad48d564c9867e
Manny <hi@mannyparra.com>
2020-09-15 18:29:08 -0600

    configure firebase hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/
 })
+
+const withNextEnv = nextEnv()
+module.exports = withNextEnv()
+
+const nextEnv = require('next-env')
+const dotenvLoad = require('dotenv-load')
+dotenvLoad()
+
+
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
 })
+
+
+
+
+
+
+
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import {jsx} from 'theme-ui'
 import {ThemeProvider} from 'theme-ui'
 import theme from '../theme'
 import React, {useEffect} from 'react'
-import firebase from '../firebase.config'
+// import firebase from '../firebase.config'
 import Nav from '../src/components/Nav'
 import Sidebar from '../src/components/Sidebar'
 import Main from '../src/components/Main'
@@ -12,22 +12,21 @@ import {Layout} from '../src/components/blog/Layout'
 
 export default function MyApp({Component, pageProps}) {
 
-  useEffect(() => {
-
-        const createMsg =
-            async () => {
-              await firebase.firestore().collection('test').add({
-                channel: 'channel2',
-                msg: 'msg2'
-              })
-            }
-        createMsg().then(results => results)
-      }, []
-  )
+  // useEffect(() => {
+  //
+  //       const createMsg =
+  //           async () => {
+  //             await firebase.firestore().collection('test').add({
+  //               channel: 'channel2',
+  //               msg: 'msg2'
+  //             })
+  //           }
+  //       createMsg().then(results => results)
+  //     }, []
+  // )
 
   return (
       <ThemeProvider theme={theme}>
-
         <Component {...pageProps} />
       </ThemeProvider>
   )
