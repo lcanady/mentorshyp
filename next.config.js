bf49c54934ce5c6f87ad69e3e2c3f7d8e520a463
Manny <hi@mannyparra.com>
2020-09-15 21:43:11 -0600

    test firebase cloud functions

--- next.config.js
+++ next.config.js
@@ -2,13 +2,14 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/
 })
 
-const withNextEnv = nextEnv()
-module.exports = withNextEnv()
+
 
 const nextEnv = require('next-env')
 const dotenvLoad = require('dotenv-load')
 dotenvLoad()
 
+const withNextEnv = nextEnv()
+module.exports = withNextEnv()
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx']
