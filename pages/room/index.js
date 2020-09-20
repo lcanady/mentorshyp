ac04eb447ef20b29bd3aa11f6fe6824d6b70ecef
= <=>
2020-09-20 16:26:27 -0700

    added SSR to room ID generation

--- pages/room/index.js
+++ pages/room/index.js
@@ -1,25 +1,16 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import { v4 } from "uuid";
 
 const RoomIndex = () => {
-  const router = useRouter();
-
-  const [id, setId] = useState();
-  useEffect(() => {
-    setId(v4());
-  }, []);
-
-  return <div>{router.push("/room/" + id)}</div>;
+  return <div>&nbsp;</div>;
 };
 
-// RoomIndex.getInitialProps = ({ res }) => {
-//   if (res) {
-//     res.writeHead(301, {
-//       Location: "/room/" + v4(),
-//     });
-//     res.end();
-//   }
-// };
+RoomIndex.getSeverSideProps = ({ res }) => {
+  if (res) {
+    res.writeHead(301, {
+      Location: "/room/" + v4(),
+    });
+    res.end();
+  }
+};
 
 export default RoomIndex;
